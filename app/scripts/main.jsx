b767c51abcab917883a73a19c8d45b2e132b9899
Andy Shu Xin <andyshuxin@gmail.com>
2015-08-04 19:40:35 +0800

    unify space

--- app/scripts/main.jsx
+++ app/scripts/main.jsx
@@ -68,37 +68,37 @@ var QuestionPanel = React.createClass({
     // Disable all checkboxes
     var checkboxes = document.getElementsByClassName("checkbox");
     for (var i=0; i<checkboxes.length; i+=1) {
-        checkboxes[i].disabled = true;
+      checkboxes[i].disabled = true;
     }
 
     post(nkoreaTest.Card.state.questionSerial.toString(), clickedOptionTag);
   },
 
   getCorrectnessSign: function(optionTag) {
-      // Only show correctness sign after the chosen option
-      if (optionTag !== nkoreaTest.Card.state.chosenOptionTag) { return '' }
-      var correctOption = nkoreaTest.survey[nkoreaTest.Card.state.questionSerial].correctOptionTag;
-      var sign, i;
-      if (nkoreaTest.Card.state.chosenOptionTag === null) {
-          sign = '';
-      } else if (nkoreaTest.Card.state.chosenOptionTag === correctOption) {
-          sign = '✓';
-      } else {
-          sign = '✕';
-      }
+    // Only show correctness sign after the chosen option
+    if (optionTag !== nkoreaTest.Card.state.chosenOptionTag) { return '' }
+    var correctOption = nkoreaTest.survey[nkoreaTest.Card.state.questionSerial].correctOptionTag;
+    var sign, i;
+    if (nkoreaTest.Card.state.chosenOptionTag === null) {
+      sign = '';
+    } else if (nkoreaTest.Card.state.chosenOptionTag === correctOption) {
+      sign = '✓';
+    } else {
+      sign = '✕';
+    }
 
-      //Set color
-      var correctnessSignDOMNodes = document.getElementsByClassName('CorrectnessSign');
-      if (sign === '✓') {
-          for (i=0; i<correctnessSignDOMNodes.length; i+=1) {
-              correctnessSignDOMNodes[i].className = 'CorrectnessSign CorrectnessSign-correct'
-          }
-      } else if (sign === '✕') {
-          for (i=0; i<correctnessSignDOMNodes.length; i+=1) {
-              correctnessSignDOMNodes[i].className = 'CorrectnessSign CorrectnessSign-wrong'
-          }
+    //Set color
+    var correctnessSignDOMNodes = document.getElementsByClassName('CorrectnessSign');
+    if (sign === '✓') {
+      for (i=0; i<correctnessSignDOMNodes.length; i+=1) {
+        correctnessSignDOMNodes[i].className = 'CorrectnessSign CorrectnessSign-correct'
       }
-      return sign;
+    } else if (sign === '✕') {
+      for (i=0; i<correctnessSignDOMNodes.length; i+=1) {
+        correctnessSignDOMNodes[i].className = 'CorrectnessSign CorrectnessSign-wrong'
+      }
+    }
+    return sign;
   },
 
   render: function() {
@@ -109,63 +109,63 @@ var QuestionPanel = React.createClass({
 
     var optionBoxes;
     if (this.props.data.optionContainsImage) {
-        optionBoxes = this.props.data.options.map(function (option) {
-            // Special adjustment for wider images
-            var widerMarker = '';
-            if (option.optionTag === 'D') {
-                widerMarker = ' optionBoxWithImageWide'
-            }
-            return (
-                <div className={"optionBox optionBoxWithImage"+widerMarker} key={option.optionTag}>
-                    <label className="WithImage">
-                        <div className="optionBoxLeftContainer">
-                          <input type="checkbox"
-                                 name={option.optionTag}
-                                 onChange={this.handleCheckboxClick.bind(this, option.optionTag)}
-                                 className="checkbox"
-                              />
-                          <span className="optionTag">{option.optionTag}</span>
-                        </div>
-                        <img src={option.imagePath} className="insertImage"/>
-                        <div className="optionBoxRightContainer">
-                          <span className="optionText optionTextWithImage">{option.optionText}</span>
-                          <span className="CorrectnessSign">{this.getCorrectnessSign(option.optionTag)}</span>
-                        </div>
-                    </label>
-                </div>
-            )
-        }, this);
+      optionBoxes = this.props.data.options.map(function (option) {
+        // Special adjustment for wider images
+        var widerMarker = '';
+        if (option.optionTag === 'D') {
+          widerMarker = ' optionBoxWithImageWide'
+        }
+        return (
+          <div className={"optionBox optionBoxWithImage"+widerMarker} key={option.optionTag}>
+            <label className="WithImage">
+              <div className="optionBoxLeftContainer">
+                <input type="checkbox"
+                       name={option.optionTag}
+                       onChange={this.handleCheckboxClick.bind(this, option.optionTag)}
+                       className="checkbox"
+                    />
+                <span className="optionTag">{option.optionTag}</span>
+              </div>
+              <img src={option.imagePath} className="insertImage"/>
+              <div className="optionBoxRightContainer">
+                <span className="optionText optionTextWithImage">{option.optionText}</span>
+                <span className="CorrectnessSign">{this.getCorrectnessSign(option.optionTag)}</span>
+              </div>
+            </label>
+          </div>
+        )
+      }, this);
     } else {
-        optionBoxes = this.props.data.options.map(function (option) {
-            return (
-                <div className="optionBox" key={option.optionTag}>
-                    <label>
-                        <input type="checkbox"
-                               name={option.optionTag}
-                               onChange={this.handleCheckboxClick.bind(this, option.optionTag)}
-                               className="checkbox"
-                            />
-                        <span className="optionTag">{option.optionTag}</span>
-                        <span className="optionText">{option.optionText}</span>
-                        <span className="CorrectnessSign">{this.getCorrectnessSign(option.optionTag)}</span>
-                    </label>
-                </div>
-            )
-        }, this);
+      optionBoxes = this.props.data.options.map(function (option) {
+        return (
+          <div className="optionBox" key={option.optionTag}>
+            <label>
+              <input type="checkbox"
+                     name={option.optionTag}
+                     onChange={this.handleCheckboxClick.bind(this, option.optionTag)}
+                     className="checkbox"
+                  />
+              <span className="optionTag">{option.optionTag}</span>
+              <span className="optionText">{option.optionText}</span>
+              <span className="CorrectnessSign">{this.getCorrectnessSign(option.optionTag)}</span>
+            </label>
+          </div>
+        )
+      }, this);
     }
 
     return (
-        <div id="QuestionPanel" data={this.props.data}>
-          <div id="questionTag">
-            {this.props.data.questionTag}
-          </div>
-          <div id="questionText">
-            {this.props.data.questionText}
-          </div>
-          <div id="OptionPanel">
-            {optionBoxes}
-          </div>
+      <div id="QuestionPanel" data={this.props.data}>
+        <div id="questionTag">
+          {this.props.data.questionTag}
         </div>
+        <div id="questionText">
+          {this.props.data.questionText}
+        </div>
+        <div id="OptionPanel">
+          {optionBoxes}
+        </div>
+      </div>
     );
   }
 });
@@ -175,22 +175,22 @@ var AnswerPanel = React.createClass({
   handleNextButtonClick: function (event) {
     //Show the next question
     nkoreaTest.Card.setState({
-        questionSerial: nkoreaTest.Card.state.questionSerial + 1,
-        answerSerial: nkoreaTest.Card.state.answerSerial + 1,
-        showAnswer: false
+      questionSerial: nkoreaTest.Card.state.questionSerial + 1,
+      answerSerial: nkoreaTest.Card.state.answerSerial + 1,
+      showAnswer: false
       }
     );
 
     if (nkoreaTest.Card.state.questionSerial === nkoreaTest.survey.length - 2) {
       nkoreaTest.Card.setState({
-          lastQuestion: true
+        lastQuestion: true
       });
     }
 
     if (nkoreaTest.Card.state.questionSerial >= nkoreaTest.survey.length - 1) {
       React.render(
-          <ScorePage />,
-          document.getElementById('content')
+        <ScorePage />,
+        document.getElementById('content')
       );
       post("result", nkoreaTest.totalScore.toString());
     }
@@ -209,26 +209,26 @@ var AnswerPanel = React.createClass({
 
   render: function() {
     return (
-        <div id="AnswerPanel" data={this.props.data} key={0}>
-          <div className="ExplanationBox">
-            <div className="answerBox">
-                <div className="answerInnerBox">
-                  <span className="answerLabel">
-                    {nkoreaTest.text.answerLabel}
-                  </span>
-                  <span className="answerTag">
-                    {this.props.data.correctOptionTag}
-                  </span>
-                </div>
+      <div id="AnswerPanel" data={this.props.data} key={0}>
+        <div className="ExplanationBox">
+          <div className="answerBox">
+            <div className="answerInnerBox">
+              <span className="answerLabel">
+                {nkoreaTest.text.answerLabel}
+              </span>
+              <span className="answerTag">
+                {this.props.data.correctOptionTag}
+              </span>
             </div>
-            <span className="Explanation">{this.props.data.ExplanationText}</span>
-            <button id="next" onClick={this.handleNextButtonClick}>
-                {nkoreaTest.Card.state.lastQuestion ?
-                    nkoreaTest.text.lastButtonLabel:
-                    nkoreaTest.text.nextButtonLabel}
-            </button>
           </div>
+          <span className="Explanation">{this.props.data.ExplanationText}</span>
+          <button id="next" onClick={this.handleNextButtonClick}>
+            {nkoreaTest.Card.state.lastQuestion ?
+                nkoreaTest.text.lastButtonLabel:
+                nkoreaTest.text.nextButtonLabel}
+          </button>
         </div>
+      </div>
     );
   }
 });
@@ -247,16 +247,16 @@ var Card = React.createClass({
   render: function() {
     nkoreaTest.Card = this;
     return (
-        <div id="Card" surveyData={this.props.surveyData}>
-          <Banner />
-          <QuestionPanel data={this.props.surveyData[this.state.questionSerial]}
-          />
-          {this.state.showAnswer ?
-              <AnswerPanel data={this.props.surveyData[this.state.answerSerial]}
-              /> :
-              null
-          }
-        </div>
+      <div id="Card" surveyData={this.props.surveyData}>
+        <Banner />
+        <QuestionPanel data={this.props.surveyData[this.state.questionSerial]}
+        />
+        {this.state.showAnswer ?
+            <AnswerPanel data={this.props.surveyData[this.state.answerSerial]}
+            /> :
+            null
+        }
+      </div>
     );
   }
 });
@@ -264,21 +264,21 @@ var Card = React.createClass({
 var ScorePage = React.createClass({
 
   shareToWeibo: function () {
-      var title = encodeURIComponent(nkoreaTest.text.scoreDescription + nkoreaTest.totalScore.toString() + nkoreaTest.text.shareHint),
-          url = encodeURIComponent(nkoreaTest.url);
-      window.open('http://v.t.sina.com.cn/share/share.php?title='+title+'&url='+nkoreaTest.url);
+    var title = encodeURIComponent(nkoreaTest.text.scoreDescription + nkoreaTest.totalScore.toString() + nkoreaTest.text.shareHint),
+        url = encodeURIComponent(nkoreaTest.url);
+    window.open('http://v.t.sina.com.cn/share/share.php?title='+title+'&url='+nkoreaTest.url);
   },
 
   shareToFacebook () {
-      var description = encodeURIComponent(nkoreaTest.text.scoreDescription + nkoreaTest.totalScore.toString() + nkoreaTest.text.shareHint),
-          url = encodeURIComponent(nkoreaTest.url);
-      window.open('https://www.facebook.com/dialog/feed?app_id=743206445788490+' +
-                  '&link=' + url +
-                  '&picture=' + nkoreaTest.url + nkoreaTest.shareImgRelativePath +
-                  '&name=' + nkoreaTest.title +
-                  '&description=' + description +
-                  '&redirect_uri=' + url
-      );
+    var description = encodeURIComponent(nkoreaTest.text.scoreDescription + nkoreaTest.totalScore.toString() + nkoreaTest.text.shareHint),
+        url = encodeURIComponent(nkoreaTest.url);
+    window.open('https://www.facebook.com/dialog/feed?app_id=743206445788490+' +
+                '&link=' + url +
+                '&picture=' + nkoreaTest.url + nkoreaTest.shareImgRelativePath +
+                '&name=' + nkoreaTest.title +
+                '&description=' + description +
+                '&redirect_uri=' + url
+    );
   },
 
   componentDidMount: function() {
@@ -288,30 +288,30 @@ var ScorePage = React.createClass({
   render: function() {
     var comment = nkoreaTest.scoreComments[nkoreaTest.totalScore.toString()];
     return (
-        <div id="ScorePage">
-          <Banner />
-          <h1>你的總分是：{nkoreaTest.totalScore}</h1>
-          <p>{comment}</p>
-          <button className="share Facebook-Share-btn"
-                  onClick={this.shareToFacebook}>
-              <img src="img/FB-f-Logo__blue_50.png" />
-              {nkoreaTest.text.facebookShareButtonText}
-          </button>
-          <button className="share Weibo-Share-btn"
-                  onClick={this.shareToWeibo}>
-              <img src="img/weibo_48x48.png" />
-              {nkoreaTest.text.ShareToWeiboText}
-          </button>
+      <div id="ScorePage">
+        <Banner />
+        <h1>你的總分是：{nkoreaTest.totalScore}</h1>
+        <p>{comment}</p>
+        <button className="share Facebook-Share-btn"
+                onClick={this.shareToFacebook}>
+          <img src="img/FB-f-Logo__blue_50.png" />
+          {nkoreaTest.text.facebookShareButtonText}
+        </button>
+        <button className="share Weibo-Share-btn"
+                onClick={this.shareToWeibo}>
+          <img src="img/weibo_48x48.png" />
+          {nkoreaTest.text.ShareToWeiboText}
+        </button>
 
-          <div className='fulltextRecommendation'>
-              {nkoreaTest.text.fulltextRecommendation}
-              <a href="https://theinitium.com/project/20150803-dprk-youth/" target="_blank">
-                  {nkoreaTest.text.linkLabel}
-              </a>
-          </div>
-          <div id='marketingInfobox'></div>
-
+        <div className='fulltextRecommendation'>
+          {nkoreaTest.text.fulltextRecommendation}
+          <a href="https://theinitium.com/project/20150803-dprk-youth/" target="_blank">
+            {nkoreaTest.text.linkLabel}
+          </a>
         </div>
+        <div id='marketingInfobox'></div>
+
+      </div>
     );
   }
 });
@@ -319,8 +319,8 @@ var ScorePage = React.createClass({
 document.title = nkoreaTest.title;
 nkoreaTest.setNewUUID();
 React.render(
-    <Card surveyData={nkoreaTest.survey} />,
-    //<ScorePage />,
-    document.getElementById('content')
+  <Card surveyData={nkoreaTest.survey} />,
+  //<ScorePage />,
+  document.getElementById('content')
 );
 
